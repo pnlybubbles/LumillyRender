f8fef3993c23a2ffb069a1b0ff88ebb588ff2b04
pnlybubbles <pnlybubbles@gmail.com>
2016-06-10 14:30:40 +0900

    refactor: refresh codes and add comments

--- src/main.rs
+++ src/main.rs
@@ -85,10 +85,10 @@ struct Ray {
 #[derive(Debug, Copy, Clone, Default)]
 struct Material {
   color: Vector,
+  emission: Vector,
   diffuse: f64,
   reflection: f64,
   refraction: f64,
-  emmisive: f64,
 }
 
 #[derive(Debug, Copy, Clone, Default)]
@@ -132,14 +132,19 @@ impl Shape for Sphere {
       i.cross = false;
       return i;
     }
-    let t = -cod - det.sqrt();
-    if t < 0.0 {
+    let t1 = -cod - det.sqrt();
+    let t2 = -cod + det.sqrt();
+    if t1 < EPS && t2 < EPS {
       i.cross = false;
       return i;
     }
+    if t1 > EPS {
+      i.t = t1;
+    } else {
+      i.t = t2;
+    }
     i.cross = true;
-    i.t = t;
-    i.position = &r.o + &r.d.smul(t);
+    i.position = &r.o + &r.d.smul(i.t);
     i.normal = (&i.position - &self.position).norm();
     i.material = self.material;
     return i;
@@ -176,7 +181,7 @@ impl Shape for Triangle {
       return i;
     }
     let t = (&self.position0 - &r.o).dot(&self.normal) / dn;
-    if t < 0.0 {
+    if t < EPS {
       i.cross = false;
       return i;
     }
@@ -209,25 +214,25 @@ impl Shape for Triangle {
 struct Objects {
   triangles: Vec<Triangle>,
   spheres: Vec<Sphere>,
-  emmisive_triangles: Vec<Triangle>,
-  emmisive_triangles_area: Vec<f64>,
-  emmisive_triangles_area_total: f64,
+  emission_triangles: Vec<Triangle>,
+  emission_triangles_area: Vec<f64>,
+  emission_triangles_area_total: f64,
 }
 
 impl Objects {
   fn new(triangles: &[Triangle], spheres: &[Sphere]) -> Objects {
-    let mut emmisive_triangles: Vec<Triangle> = Default::default();
-    let mut emmisive_triangles_area: Vec<f64> = Default::default();
+    let mut emission_triangles: Vec<Triangle> = Default::default();
+    let mut emission_triangles_area: Vec<f64> = Default::default();
     let mut all_triangles: Vec<Triangle> = Default::default();
     let mut all_spheres: Vec<Sphere> = Default::default();
-    let mut emmisive_triangles_area_total = 0.0;
+    let mut emission_triangles_area_total = 0.0;
     for v in triangles {
-      if v.material.emmisive > 0.0 {
-        emmisive_triangles.push(*v);
+      if v.material.emission.dot(&v.material.emission) > 0.0 {
+        emission_triangles.push(*v);
         let cr = (&v.position1 - &v.position0).cross(&v.position2 - &v.position0);
         let area = 0.5 * cr.dot(&cr).sqrt();
-        emmisive_triangles_area.push(area);
-        emmisive_triangles_area_total += area;
+        emission_triangles_area.push(area);
+        emission_triangles_area_total += area;
       }
       all_triangles.push(*v);
     }
@@ -237,9 +242,9 @@ impl Objects {
     Objects {
       triangles: all_triangles,
       spheres: all_spheres,
-      emmisive_triangles: emmisive_triangles,
-      emmisive_triangles_area: emmisive_triangles_area,
-      emmisive_triangles_area_total: emmisive_triangles_area_total,
+      emission_triangles: emission_triangles,
+      emission_triangles_area: emission_triangles_area,
+      emission_triangles_area_total: emission_triangles_area_total,
     }
   }
 
@@ -261,12 +266,12 @@ impl Objects {
     return intersect;
   }
 
-  fn get_emmisive_point(&self) -> Vector {
-    let roulette = &self.emmisive_triangles_area_total * rand::random::<f64>();
+  fn get_emission_point(&self) -> Vector {
+    let roulette = &self.emission_triangles_area_total * rand::random::<f64>();
     let mut area = 0.0;
     let mut ret: Vector = Default::default();
-    for (i, obj) in (&self.emmisive_triangles).iter().enumerate() {
-      area += (&self.emmisive_triangles_area)[i];
+    for (i, obj) in (&self.emission_triangles).iter().enumerate() {
+      area += (&self.emission_triangles_area)[i];
       if roulette <= area {
         let mut s = rand::random::<f64>();
         let mut t = rand::random::<f64>();
@@ -284,9 +289,9 @@ impl Objects {
     return ret;
   }
 
-  fn get_emmisive_solid_angle(&self, position: Vector) -> f64 {
+  fn get_emission_solid_angle(&self, position: Vector) -> f64 {
     let mut solid_angle = 0.0;
-    for obj in &self.emmisive_triangles {
+    for obj in &self.emission_triangles {
       let pe0 = (&obj.position0 - &position).norm();
       let pe1 = (&obj.position1 - &position).norm();
       let pe2 = (&obj.position2 - &position).norm();
@@ -311,159 +316,77 @@ fn to_int(x: f64) -> i64 {
   return (clamp(x).powf(1.0 / 2.2) * 255.0) as i64
 }
 
-fn get_light(r: Ray, depth: usize, no_emmisive: bool) -> Vector{
-  if depth >= MAX_DEPTH {
-    return Vector{x: 0.0, y: 0.0, z: 0.0};
-  }
+fn radiance(r: Ray, depth: usize) -> Vector{
+  // すべてのオブジェクトと当たり判定を行う
   let i = OBJECTS.get_intersect(r);
+  // 当たらなかった場合は背景色を返す
   if !i.cross {
     return BG_COLOR;
   }
-  let cd = i.material.diffuse;
-  let cr = i.material.reflection;
-  let mut cn = i.material.refraction;
-  let ce = i.material.emmisive;
-  let roulette = (cd + cr + cn + ce) * rand::random::<f64>();
-  let mut tp = 3; // emmisive
-  if roulette < cd {
-    // diffuse
-    tp = 0;
-  } else if roulette < cd + cr {
-    // reflection
-    tp = 1;
-  } else if roulette < cd + cr + cn {
-    // refraction
-    tp = 2;
+  // 再帰抑制用のロシアンルーレットの確率を決定する
+  // 鏡面反射、屈折の時は必ず次のパスをトレースする
+  // 拡散反射の時は各色の反射率のうち最大のものを使う
+  let mut continue_rr_prob = if i.material.reflection > 0.0 || i.material.refraction > 0.0 { 1.0 } else {
+    i.material.color.x.max(i.material.color.y).max(i.material.color.z)
+  };
+  // スタックオーバーフロー対策のために反射回数の限界値を超えたら極端に確率を下げる
+  if depth > DEPTH_LIMIT {
+    continue_rr_prob *= (0.5f64).powi((depth - DEPTH_LIMIT) as i32);
   }
-
-  // diffuse
-  let mut diffuse_color = Vector{x: 0.0, y: 0.0, z: 0.0};
-  if tp == 0 {
-    // emmisive direction
-    let emmisive_position = OBJECTS.get_emmisive_point();
-    let ip_ep = &emmisive_position - &i.position;
-    let d_e = ip_ep.norm();
-    let test_ray = Ray{
-      o: &i.position + &d_e.smul(0.01),
-      d: d_e,
-    };
-    let test_i = OBJECTS.get_intersect(test_ray);
-    if test_i.cross && test_i.material.emmisive == 1.0 {
-      let prob = OBJECTS.get_emmisive_solid_angle(i.position) / (2.0 * PI);
-      let mut ip_ep_n = d_e.dot(&i.normal);
-      if ip_ep_n < 0.0 {
-        ip_ep_n = 0.0;
-      }
-      diffuse_color = (&test_i.material.color * &i.material.color).smul(prob * ip_ep_n);
-    }
-    // other direction
-
-    // orthonormal coordinate with cos importance sampling
-    // let r1: f64 = 2.0 * PI* rand::random::<f64>();
-    // let r2: f64 = rand::random::<f64>();
-    // let r2s: f64 = r2.sqrt();
-
-    // let w = i.normal;
-    // let u = if w.x.abs() > 0.1 { Vector{x: 0.0, y: 1.0, z: 0.0} } else { Vector{x: 1.0, y: 0.0, z: 0.0 } }.cross(w).norm();
-    // let v = w.cross(u);
-
-    // let d = (&(&u.smul(r1.cos() * r2s) + &v.smul(r1.sin() * r2s)) + &w.smul((1.0 - r2).sqrt())).norm();
-    // let new_ray = Ray{d: d, o: &i.position + &d.smul(0.01)};
-    // diffuse_color = &diffuse_color + &(&get_light(new_ray, depth + 1, true) * &i.material.color);
-
-    // orthonormal coordinate
+  // 最初の数回の反射では必ず次のパスをトレースするようにする
+  if depth <= DEPTH {
+    continue_rr_prob = 1.0;
+  }
+  // ロシアンルーレットで再帰を抑制
+  if continue_rr_prob != 1.0 && rand::random::<f64>() >= continue_rr_prob {
+    return i.material.emission;
+  }
+  // 拡散反射、鏡面反射、屈折のそれぞれの割合からどの処理を行うかをロシアンルーレットで決定する
+  let brdf_type_rr = (i.material.diffuse + i.material.reflection + i.material.refraction) * rand::random::<f64>();
+  let mut brdf_type_rr_prob = i.material.refraction;
+  let mut brdf_type = 2;
+  if brdf_type_rr < i.material.diffuse {
+    brdf_type_rr_prob = i.material.diffuse;
+    brdf_type = 0;
+  } else if brdf_type_rr < i.material.diffuse + i.material.reflection {
+    brdf_type_rr_prob = i.material.reflection;
+    brdf_type = 1;
+  }
+  // 拡散反射
+  if brdf_type == 0 {
+    // 乱数を生成(半球上で一様分布)
     let theta = PI * rand::random::<f64>() * 0.5;
     let phi = 2.0 * PI * rand::random::<f64>();
 
+    // 反射点での法線方向を基準にした正規直交基底を生成
     let w = i.normal;
     let u = if w.x.abs() > 0.1 { Vector{x: 0.0, y: 1.0, z: 0.0} } else { Vector{x: 1.0, y: 0.0, z: 0.0 } }.cross(w);
     let v = w.cross(u);
 
-    let d = (&(&u.smul(theta.sin() * phi.cos()) + &v.smul(theta.sin() * phi.sin())) + &w.smul(theta.cos())).norm();
+    // 球面極座標を用いて反射点から単位半球面上のある一点へのベクトルを生成
+    let d = &(&u.smul(theta.sin() * phi.cos()) + &v.smul(theta.sin() * phi.sin())) + &w.smul(theta.cos());
+    // cos項を計算
     let dn = d.dot(&i.normal);
-    let new_ray = Ray{d: d, o: &i.position + &d.smul(0.01)};
-    diffuse_color = &diffuse_color + &(&get_light(new_ray, depth + 1, true) * &i.material.color).smul(PI / dn);
-
-    // normal inversing
-    // let theta = PI * rand::random::<f64>();
-    // let phi = 2.0 * PI * rand::random::<f64>();
-    // let mut d = Vector{x: theta.sin() * phi.cos(), y: theta.sin() * phi.sin(), z: theta.cos()};
-    // let mut dn = d.dot(&i.normal);
-    // if dn < 0.0 {
-    //   dn = -dn;
-    //   d = d.smul(-1.0);
-    // }
-    // let new_ray = Ray{d: d, o: &i.position + &d.smul(0.01)};
-    // diffuse_color = &diffuse_color + &(&get_light(new_ray, depth + 1, true) * &i.material.color).smul(PI / dn);
+    // 新しいレイを作る
+    let new_ray = Ray{d: d, o: i.position};
+    // BRDFは半球全体に一様分布なので σ / π
+    let brdf = i.material.color.smul(1.0 / PI);
+    // 確率密度関数はサンプルした乱数が半球上で一様分布なので 1 / 2π
+    let pdf = 1.0 / (2.0 * PI);
+    // レンダリング方程式にしたがって放射輝度を計算する
+    // ロシアンルーレットを用いた評価で期待値を満たすために確率で割る (BRDFタイプ用と再帰抑制用)
+    // L_e + BRDF * L_i * cosθ / (PDF * RR_prob)
+    return &i.material.emission + &(&brdf * &radiance(new_ray, depth + 1).smul(dn / (pdf * continue_rr_prob * brdf_type_rr_prob)));
   }
-
-  // reflection
-  let mut reflection_color = Vector{x: 0.0, y: 0.0, z: 0.0};
-  if tp == 1 {
-    let d = &r.d - &i.normal.smul(2.0 * r.d.dot(&i.normal));
-    let new_ray = Ray{d: d, o: &i.position + &d.smul(0.01)};
-    reflection_color = get_light(new_ray, depth, false);
-  }
-
-  // refraction
-  let mut refraction_color = Vector{x: 0.0, y: 0.0, z: 0.0};
-  if tp == 2 {
-    let mut d = Vector{x: 0.0, y: 0.0, z: 0.0};
-    let mut o = Vector{x: 0.0, y: 0.0, z: 0.0};
-    let dn = r.d.dot(&i.normal);
-    let eta = 1.5;
-    if dn < 0.0{
-      // incidence
-      let det = 1.0 - (1.0 + dn) * (1.0 + dn) / (eta * eta);
-      if det < 0.0 {
-        cn = 0.0;
-      } else {
-        d = &i.normal.smul(-dn / eta - det.sqrt()) + &r.d.smul(1.0 / eta);
-        o = &i.position - &i.normal.smul(0.01)
-      }
-    } else {
-      // outgoing
-      let det = 1.0 - (1.0 - dn) * (1.0 - dn) * (eta * eta);
-      if det < 0.0 {
-        cn = 0.0;
-      } else {
-        d = &i.normal.smul(-(dn * eta - det.sqrt())) + &r.d.smul(eta);
-        o = &i.position + &i.normal.smul(0.01)
-      }
-    }
-    if cn != 0.0 {
-      let new_ray = Ray{d: d.norm(), o: o};
-      refraction_color = get_light(new_ray, depth, false);
-    }
-  }
-
-  // emmisive
-  let mut emmisive_color = Vector{x: 0.0, y: 0.0, z: 0.0};
-  if tp == 3 {
-    if !no_emmisive {
-      emmisive_color = i.material.color;
-    }
-  }
-
-  return if tp == 0 {
-    diffuse_color.smul(cd + cr + cn)
-  } else if tp == 1 {
-    reflection_color.smul(cd + cr + cn)
-  } else if tp == 2 {
-    refraction_color.smul(cd + cr + cn)
-  } else if tp == 3 {
-    emmisive_color.smul(ce)
-  } else {
-    Vector{x: 0.0, y: 0.0, z: 0.0}
-  }
+  return Vector{x: 0.0, y: 0.0, z: 0.0};
 }
 
-const YELLOW_MATERIAL: Material = Material{diffuse: 1.0, reflection: 0.0, refraction: 0.0, emmisive: 0.0, color: Vector{x: 1.0, y: 1.0, z: 0.5}};
-const BLUE_MATERIAL: Material = Material{diffuse: 1.0, reflection: 0.0, refraction: 0.0, emmisive: 0.0, color: Vector{x: 0.5, y: 0.5, z: 1.0}};
-const WHITE_MATERIAL: Material = Material{diffuse: 1.0, reflection: 0.0, refraction: 0.0, emmisive: 0.0, color: Vector{x: 1.0, y: 1.0, z: 1.0}};
-const REFLECTION_MATERIAL: Material = Material{diffuse: 0.1, reflection: 0.9, refraction: 0.0, emmisive: 0.0, color: Vector{x: 0.0, y: 0.0, z: 0.0}};
-const REFRACTION_MATERIAL: Material = Material{diffuse: 0.0, reflection: 0.2, refraction: 0.8, emmisive: 0.0, color: Vector{x: 0.0, y: 0.0, z: 0.0}};
-const EMMISIVE_MATERIAL: Material = Material{diffuse: 0.0, reflection: 0.0, refraction: 0.0, emmisive: 1.0, color: Vector{x: 11.0, y: 11.0, z: 11.0}};
+const YELLOW_MATERIAL: Material = Material{diffuse: 1.0, reflection: 0.0, refraction: 0.0, emission: Vector{x: 0.0, y: 0.0, z: 0.0}, color: Vector{x: 1.0, y: 1.0, z: 0.5}};
+const BLUE_MATERIAL: Material = Material{diffuse: 1.0, reflection: 0.0, refraction: 0.0, emission: Vector{x: 0.0, y: 0.0, z: 0.0}, color: Vector{x: 0.5, y: 0.5, z: 1.0}};
+const WHITE_MATERIAL: Material = Material{diffuse: 1.0, reflection: 0.0, refraction: 0.0, emission: Vector{x: 0.0, y: 0.0, z: 0.0}, color: Vector{x: 1.0, y: 1.0, z: 1.0}};
+const REFLECTION_MATERIAL: Material = Material{diffuse: 0.0, reflection: 1.0, refraction: 0.0, emission: Vector{x: 0.0, y: 0.0, z: 0.0}, color: Vector{x: 0.0, y: 0.0, z: 0.0}};
+const REFRACTION_MATERIAL: Material = Material{diffuse: 0.0, reflection: 0.0, refraction: 1.0, emission: Vector{x: 0.0, y: 0.0, z: 0.0}, color: Vector{x: 0.0, y: 0.0, z: 0.0}};
+const EMISSION_MATERIAL: Material = Material{diffuse: 1.0, reflection: 0.0, refraction: 0.0, emission: Vector{x: 3.0, y: 3.0, z: 3.0}, color: Vector{x: 1.0, y: 1.0, z: 1.0}};
 
 lazy_static! {
   static ref TRIANGLE_OBJECTS: [Triangle; 14] = [
@@ -479,22 +402,24 @@ lazy_static! {
     Triangle::new(Vector{x: -5.0, y: -5.0, z: 0.0}, Vector{x: 5.0, y: -5.0, z: 0.0}, Vector{x: 5.0, y: -5.0, z: -10.0}, WHITE_MATERIAL),
     Triangle::new(Vector{x: -5.0, y: 5.0, z: 0.0}, Vector{x: -5.0, y: 5.0, z: -10.0}, Vector{x: 5.0, y: 5.0, z: -10.0}, WHITE_MATERIAL),
     Triangle::new(Vector{x: 5.0, y: 5.0, z: 0.0}, Vector{x: -5.0, y: 5.0, z: 0.0}, Vector{x: 5.0, y: 5.0, z: -10.0}, WHITE_MATERIAL),
-    Triangle::new(Vector{x: -1.5, y: 4.99, z: -3.5}, Vector{x: -1.5, y: 4.99, z: -6.5}, Vector{x: 1.5, y: 4.99, z: -6.5}, EMMISIVE_MATERIAL),
-    Triangle::new(Vector{x: 1.5, y: 4.99, z: -3.5}, Vector{x: -1.5, y: 4.99, z: -3.5}, Vector{x: 1.5, y: 4.99, z: -6.5}, EMMISIVE_MATERIAL),
+    Triangle::new(Vector{x: -1.5, y: 4.99, z: -3.5}, Vector{x: -1.5, y: 4.99, z: -6.5}, Vector{x: 1.5, y: 4.99, z: -6.5}, EMISSION_MATERIAL),
+    Triangle::new(Vector{x: 1.5, y: 4.99, z: -3.5}, Vector{x: -1.5, y: 4.99, z: -3.5}, Vector{x: 1.5, y: 4.99, z: -6.5}, EMISSION_MATERIAL),
   ];
 
   static ref SPHERE_OBJECTS: [Sphere; 2] = [
-    Sphere::new(Vector{x: -2.0, y: 2.0, z: -7.0}, 1.8, REFLECTION_MATERIAL),
-    Sphere::new(Vector{x: 2.0, y: -3.2, z: -3.0}, 1.8, REFRACTION_MATERIAL),
+    Sphere::new(Vector{x: -2.0, y: 2.0, z: -7.0}, 1.8, WHITE_MATERIAL),
+    Sphere::new(Vector{x: 2.0, y: -3.2, z: -3.0}, 1.8, WHITE_MATERIAL),
   ];
 
   static ref OBJECTS: Objects = Objects::new(&*TRIANGLE_OBJECTS, &*SPHERE_OBJECTS);
 }
 
-const MAX_DEPTH: usize = 5;
-const WIDTH: usize = 512;
-const HEIGHT: usize = 512;
+const DEPTH: usize = 3;
+const DEPTH_LIMIT: usize = 5;
+const WIDTH: usize = 256;
+const HEIGHT: usize = 256;
 const PI: f64 = 3.14159265358979323846264338327950288_f64;
+const EPS: f64 = 1e-6;
 const BG_COLOR: Vector = Vector{x: 0.0, y: 0.0, z: 0.0};
 
 fn main() {
@@ -503,7 +428,7 @@ fn main() {
   let pool = ThreadPool::new(cpu_count);
   let (tx, rx): (Sender<(usize, usize, Vector)>, Receiver<(usize, usize, Vector)>) = channel();
 
-  let samples: usize = 10;
+  let samples: usize = 20;
   let mut output = box [[Vector{x: 0.0, y: 0.0, z: 0.0}; WIDTH]; HEIGHT];
   let min_rsl: f64 = cmp::min(WIDTH, HEIGHT) as f64;
 
@@ -520,7 +445,7 @@ fn main() {
             y: ((i as f64 + rand::random::<f64>()) * 2.0 - (HEIGHT as f64 + 1.0)) / min_rsl,
             z: -3.0,
           }.norm();
-          r = &r + &get_light(ray, 0, false).smul(1.0 / samples as f64);
+          r = &r + &radiance(ray, 0).smul(1.0 / samples as f64);
         }
         tx.send((i, j, Vector{x: clamp(r.x), y: clamp(r.y), z: clamp(r.z)})).unwrap();
       });
