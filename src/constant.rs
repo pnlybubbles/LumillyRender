6dd209c9c562f0ef726638c5c806b37beb80f25d
pnlybubbles <pnlybubbles@gmail.com>
2016-08-25 16:29:28 +0900

    feat: edit scene

--- src/constant.rs
+++ src/constant.rs
@@ -1,13 +1,13 @@
 pub const PI: f64 = 3.14159265358979323846264338327950288_f64;
 pub const EPS: f64 = 1e-6;
 
-pub const HEIGHT: usize = 270 * 1;
-pub const WIDTH: usize = 480 * 1;
+pub const HEIGHT: usize = 270 * 2;
+pub const WIDTH: usize = 480 * 2;
 
 pub const CROP_OFFSET_TOP: usize = 0;
 pub const CROP_OFFSET_RIGHT: usize = 0;
-pub const CROP_HEIGHT: usize = 270;
-pub const CROP_WIDTH: usize = 480;
+pub const CROP_HEIGHT: usize = 270 * 2;
+pub const CROP_WIDTH: usize = 480 * 2;
 
 pub const TIME_LIMIT: f64 = 60.0 * 5.0;
 pub const SAVE_IMAGE_INTERVAL: f64 = 30.0;
