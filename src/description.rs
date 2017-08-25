bf75231e50287fc7350355f222efa9a9261eec1a
pnlybubbles <pnlybubbles@gmail.com>
2017-08-25 18:12:53 +0900

    feat: implements ibl for skybox

--- src/description.rs
+++ src/description.rs
@@ -14,19 +14,19 @@ use sky::*;
 pub fn camera() -> Camera {
   let w = Img::width() as f64;
   let h = Img::height() as f64;
-  let cam_pos = Vector3::new(50.0, 52.0, 220.0);
-  let cam_dir = Vector3::new(0.0, -0.04, -1.0).norm();
+  let cam_pos = Vector3::new(-11.5, 1.0, 13.0);
+  let screen_pos = Vector3::new(8.18, -2.0, -9.0);
   Camera::new(
     // sensor position
-    cam_pos - cam_dir * 40.0,
+    cam_pos - screen_pos,
     // aperture position
     cam_pos,
     // sensor size
-    Vector2::new(30.0 * w / h, 30.0),
+    Vector2::new(10.0 * w / h, 10.0),
     // sensor resolution
     Vector2::new(Img::width(), Img::height()),
     // aperture radius
-    1e-5,
+    EPS,
   )
 }
 
@@ -58,23 +58,17 @@ pub fn scene() -> Scene {
     ior: INF, // sin(0) = 1.0 / ior
   });
   let spheres = vec![
-    Sphere { radius: 1e5, position: Vector3::new(1e5+1.0, 40.8, 81.6),   material: red_mat.clone() },
-    Sphere { radius: 1e5, position: Vector3::new(-1e5+99.0, 40.8, 81.6), material: blue_mat.clone() },
-    Sphere { radius: 1e5, position: Vector3::new(50.0, 40.8, 1e5),       material: white_mat.clone() },
-    Sphere { radius: 1e5, position: Vector3::new(50.0, 1e5, 81.6),       material: white_mat.clone() },
-    Sphere { radius: 1e5, position: Vector3::new(50.0, -1e5+81.6, 81.6), material: white_mat.clone() },
-    Sphere { radius: 16.5,position: Vector3::new(27.0, 16.5, 47.0),      material: mirror_mat.clone() },
-    Sphere { radius: 16.5,position: Vector3::new(77.0, 16.5, 78.0),      material: glass_mat.clone() },
-    Sphere { radius: 15.0,position: Vector3::new(50.0, 90.0, 81.6),      material: light_mat.clone() },
+    Sphere { radius: 1.8, position: Vector3::new(-4.0, -3.2, 0.5), material: glass_mat.clone() },
+    Sphere { radius: 1.8, position: Vector3::new(0.8, -3.2, -0.5), material: white_mat.clone() },
+    Sphere { radius: 1.8, position: Vector3::new(2.0, -3.2, 4.0), material: mirror_mat.clone() },
+    Sphere { radius: 1e5, position: Vector3::new(0.0, -1e5 - 5.0, 0.0), material: white_mat.clone() },
   ];
   let objects = Objects {
     objects: spheres,
   };
-  let sky = Arc::new(UniformSky {
-    emission: Vector3::new(0.0, 0.0, 0.0),
-  });
+  let sky = Arc::new(IBLSky::new("ibl.hdr", 2500));
   Scene {
-    depth: 4,
+    depth: 5,
     depth_limit: 64,
     sky: sky,
     objects: objects,
--- src/sky.rs
+++ src/sky.rs
@@ -1,6 +1,11 @@
+extern crate image;
+
 use ray::Ray;
 use vector::*;
 use vector3::Vector3;
+use constant::*;
+use std::fs::File;
+use std::io::BufReader;
 
 pub trait Sky {
   fn radiance(&self, &Ray) -> Vector3<f64>;
@@ -25,6 +30,41 @@ impl Sky for SimpleSky {
   fn radiance(&self, ray: &Ray) -> Vector3<f64> {
     let weight = ray.direction.dot(Vector3::new(0.0, 1.0, 0.0)).abs();
     self.meridian * weight + self.horizon * (1.0 - weight)
+  }
+}
+
+pub struct IBLSky {
+  hdr_image: Vec<image::Rgb<f32>>,
+  height: usize,
+  longitude_offset: usize,
+}
+
+impl IBLSky {
+  pub fn new(path: &'static str, longitude_offset: usize) -> IBLSky {
+    println!("loading hdr image...");
+    let image_file = File::open(path).unwrap();
+    let decoder = image::hdr::HDRDecoder::new(BufReader::new(image_file)).unwrap();
+    println!("{:?}", decoder.metadata());
+    let height = decoder.metadata().height as usize;
+    let image = decoder.read_image_hdr().unwrap();
+    IBLSky {
+      hdr_image: image,
+      height: height,
+      longitude_offset: longitude_offset,
+    }
+  }
+}
+
+impl Sky for IBLSky {
+  fn radiance(&self, ray: &Ray) -> Vector3<f64> {
+    let theta = (ray.direction.y).acos();
+    let phi_pr = (ray.direction.z / ray.direction.x).atan();
+    let phi = if ray.direction.x < 0.0 { phi_pr + PI } else { phi_pr } + PI / 2.0;
+    let x = (self.height as f64 * phi / PI).round() as usize + self.longitude_offset;
+    let y = (self.height as f64 * theta / PI).round() as usize;
+    let index = y * self.height * 2 + if x > self.height * 2 { x % (self.height * 2) } else { x };
+    let color = self.hdr_image[index];
+    return Vector3::new(color.data[0] as f64, color.data[1] as f64, color.data[2] as f64);
   }
 }
 
